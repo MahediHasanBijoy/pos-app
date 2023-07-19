435ebcb0745052512f85c6898159efe247633f35
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-07-19 23:33:06 +0600

    jwt auth

---
+++ app/Helpers/JWTToken.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Helpers;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken{
+    public static function CreateToken($userEmail):string{
+        $key = env('JWT_KEY');
+        $payload=[
+            'iss'=>'laravel-token',
+            'iat'=>time(),
+            'exp'=>time()+60*60,
+            'userEmail'=>$userEmail
+        ];
+       return JWT::encode($payload,$key,'HS256');
+    }
+
+    public static function CreateTokenForSetPassword($userEmail):string{
+        $key =env('JWT_KEY');
+        $payload=[
+            'iss'=>'laravel-token',
+            'iat'=>time(),
+            'exp'=>time()+60*20,
+            'userEmail'=>$userEmail,
+            'userID'=>'0'
+        ];
+        return JWT::encode($payload,$key,'HS256');
+    }
+
+
+
+    public static function VerifyToken($token):object|string
+    {
+        try {
+            if($token==null){
+                return 'unauthorized';
+            }
+            else{
+                $key =env('JWT_KEY');
+                $decode=JWT::decode($token,new Key($key,'HS256'));
+                return $decode;
+            }
+        }
+        catch (Exception $e){
+            return 'unauthorized';
+        }
+    }
+
+}
